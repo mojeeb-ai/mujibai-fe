1625aa11a1a998d93c3d27370a0d7ebe451dbf6d
alaaadeln7 <alaaadeln7@gmail.com>
2025-11-24 02:44:48 +0200

    remove api dir

--- apis/api.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosAPI } from "@/axios/axiosInstance";
-
-export const getSections = async () => {
-  const response = await AxiosAPI.get(`/api/v1/sections/`);
-  return response.data;
-};
